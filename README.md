3902197b529291d07f66f60ea4a59b16d90b771b
Andreas Karlsson <andreas.karlsson@indiebytes.se>
2015-04-16 15:47:13 +0200

    Add the basic stuff like Composer, Codeception and modman

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# KL_BlockEndpoint
-Magento module for loading blocks with AJAX
+# Block Endpoint for Magento
+
+#### Karlsson & Lord
+
+[![Build Status](https://magnum.travis-ci.com/karlssonlord/KL_BlockEndpoint.svg?token=sQXdpxAyyPpb144GJqMU)](https://magnum.travis-ci.com/karlssonlord/KL_BlockEndpoint)
---
+++ src/app/code/community/KL/BlockEndpoint/Helper/Data.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * BlockEndpoint
+ *
+ * @category  KL
+ * @package   KL_BlockEndpoint
+ * @author    Andreas Karlsson <andreas@karlssonlord.com>
+ * @copyright 2015 Karlsson & Lord AB
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+/**
+ * Data helper
+ *
+ * @category  KL
+ * @package   KL_BlockEndpoint
+ * @author    Andreas Karlsson <andreas@karlssonlord.com>
+ * @copyright 2015 Karlsson & Lord AB
+ * @license   http://opensource.org/licenses/MIT
+ */
+class KL_BlockEndpoint_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
